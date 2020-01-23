bbd912feb9f7615a09c885edc42bf3780d8ef1eb
Jaxon Willard <jaxonbwillard@gmail.com>
2020-01-23 11:49:16 -0700

    finished the java part TODO: figure out GRADLE!

--- src/Main.java
+++ src/Main.java
@@ -31,21 +31,29 @@ public class Main {
     public static void usage(){
     }
     public static void CPU(){
-        System.out.println();
+        System.out.println("Processors           : " + Runtime.getRuntime().availableProcessors());
     }
     public static void mem(){
-        System.out.println("Free Memory:     " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().freeMemory()));
-        System.out.println("Total Memory:    " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().totalMemory()));
-        System.out.println("Total Memory:    " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().maxMemory()));
+        System.out.println("Free Memory  :     " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().freeMemory()));
+        System.out.println("Total Memory :    " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().totalMemory()));
+        System.out.println("Max Memory   :    " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().maxMemory()));
 
     }
     public static void dirs(){
+        Properties properties = new Properties();
+        System.out.println("Working Directory    : " + System.getProperty("user.dir"));
+        System.out.println("User Home Directory  : " + System.getProperty("user.home"));
+    }
+    public static void java(){
+        System.out.println("Java Vendor          :  " + System.getProperty("java.vendor"));
+        System.out.println("Java Runtime         :  " + System.getProperty("java.runtime.name"));
+        System.out.println("Java Version         :  " + System.getProperty("java.version"));
+        System.out.println("Java VM Version      :  " + System.getProperty("java.vm.version"));
+        System.out.println("Java VM Name         :  " + System.getProperty("java.vm.name"));
 
     }
     public static void os(){
-
-    }
-    public static void java(){
-
+        System.out.println("OS Name        : " + System.getProperty("os.name"));
+        System.out.println("OS Version     : " + System.getProperty("os.version"));
     }
 }
