f52f28d0e9dff5786d4b3504c552397f2d109c71
Jaxon Willard <jaxonbwillard@gmail.com>
2020-01-23 18:42:11 -0700

    touch up

--- src/Main.java
+++ src/Main.java
@@ -21,30 +21,25 @@ public class Main {
                 case "-java":
                     java();
                     break;
-                default:
-                    System.out.println("Incorrect argument: " + arg);
-                    usage();
-                    break;
             }
         }
     }
-    public static void usage(){
-    }
-    public static void CPU(){
+
+    private static void CPU(){
         System.out.println("Processors           : " + Runtime.getRuntime().availableProcessors());
     }
-    public static void mem(){
+    private static void mem(){
         System.out.println("Free Memory  :     " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().freeMemory()));
         System.out.println("Total Memory :    " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().totalMemory()));
         System.out.println("Max Memory   :    " + NumberFormat.getNumberInstance(Locale.US).format(Runtime.getRuntime().maxMemory()));
 
     }
-    public static void dirs(){
+    private static void dirs(){
         Properties properties = new Properties();
         System.out.println("Working Directory    : " + System.getProperty("user.dir"));
         System.out.println("User Home Directory  : " + System.getProperty("user.home"));
     }
-    public static void java(){
+    private static void java(){
         System.out.println("Java Vendor          :  " + System.getProperty("java.vendor"));
         System.out.println("Java Runtime         :  " + System.getProperty("java.runtime.name"));
         System.out.println("Java Version         :  " + System.getProperty("java.version"));
@@ -52,7 +47,7 @@ public class Main {
         System.out.println("Java VM Name         :  " + System.getProperty("java.vm.name"));
 
     }
-    public static void os(){
+    private static void os(){
         System.out.println("OS Name        : " + System.getProperty("os.name"));
         System.out.println("OS Version     : " + System.getProperty("os.version"));
     }
